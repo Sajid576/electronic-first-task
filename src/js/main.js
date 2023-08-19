9934834395e73257e323e32404fa151171699f07
sajid576 <sajidahmed696@gmail.com>
2023-08-20 00:27:23 +0600

    Add js event handler methods

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,32 @@
 
+const searchBtn= document.getElementById('search-bar');
+const navHeader= document.getElementById('nav-header');
+const searchBox= document.getElementById('text-container');
+const closeBtn= document.getElementById('close-button');
+const dropDownBtn= document.getElementById('drop-down');
 
-const slider = document.querySelector('.slider');
-let isDragging = false;
-let startPosition = 0;
-let currentTranslate = 0;
-let prevTranslate = 0;
+searchBtn.addEventListener('click',()=>{
+    navHeader.style.display='none';
+    searchBtn.style.display='none';
+    searchBox.style.display='flex';
+})
 
-slider.addEventListener('mousedown', (e) => {
-  isDragging = true;
-  startPosition = e.clientX - slider.offsetLeft;
-  prevTranslate = currentTranslate;
+navHeader.addEventListener('click',()=>{
+  if(dropDownBtn.style.display=='none'){
+    dropDownBtn.style.display='flex';
+  }
+  else{
+    dropDownBtn.style.display='none';
+  }
+  
+   
+})
 
-  slider.style.transition = 'none';
-});
+closeBtn.addEventListener('click',()=>{
+    navHeader.style.display='flex';
+    searchBtn.style.display='flex';
+    searchBox.style.display='none';
+})
 
-slider.addEventListener('mousemove', (e) => {
-  if (!isDragging) return;
-  const currentPosition = e.clientX - slider.offsetLeft;
-  currentTranslate = prevTranslate + currentPosition - startPosition;
-});
 
-slider.addEventListener('mouseup', () => {
-  isDragging = false;
-  slider.style.transition = 'transform 0.3s ease-in-out';
-  prevTranslate = currentTranslate;
 
-  // Snap to the nearest product
-  const snapToProduct = Math.round(currentTranslate / slider.offsetWidth) * slider.offsetWidth;
-  currentTranslate = snapToProduct;
-
-  slider.style.transform = `translateX(${currentTranslate}px)`;
-});
-
-slider.addEventListener('mouseleave', () => {
-  isDragging = false;
-  slider.style.transition = 'transform 0.3s ease-in-out';
-  currentTranslate = prevTranslate;
-
-  slider.style.transform = `translateX(${currentTranslate}px)`;
-});
